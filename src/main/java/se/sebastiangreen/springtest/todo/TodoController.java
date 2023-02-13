b4e9643696146339e67d0dcc398f8bd87e04d4a6
Sebastian Green <sgre@netlight.com>
2023-02-13 15:36:37 +0100

    Retrieve todo by id

--- src/main/java/se/sebastiangreen/springtest/todo/TodoController.java
+++ src/main/java/se/sebastiangreen/springtest/todo/TodoController.java
@@ -2,11 +2,15 @@ package se.sebastiangreen.springtest.todo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 
 @RestController
@@ -19,16 +23,28 @@ public class TodoController {
     }
 
     @GetMapping("")
-    public List<Todo> getMethodName() {
-        List<Todo> todos = new ArrayList<Todo>();
+    public List<Todo> getAll() {
+        var todos = new ArrayList<Todo>();
         this.repository.findAll().forEach(todos::add);
 
         return todos;
     }
 
+    @GetMapping("{id}")
+    public ResponseEntity<Todo> getById(@PathVariable UUID id) {
+        var todo = this.repository.findById(id);
+
+        if (todo.isPresent()){
+            return new ResponseEntity<Todo>(todo.get(), HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
     @PostMapping("seed")
     public void addABunch(){
-        List<Todo> todos = new ArrayList<Todo>();
+        var todos = new ArrayList<Todo>();
         todos.add(new Todo("Implement repository"));
         todos.add(new Todo("Generate db migration"));
         todos.add(new Todo("Add post and get by id endpoints"));
